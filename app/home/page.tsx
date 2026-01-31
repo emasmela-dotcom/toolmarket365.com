7b55f013bb6ce502bb35b1164cb416d478471b74
Eric <emasmela1976@gmail.com>
2026-01-31 16:34:49 -0500

    Serve real homepage: rewrite / to /home so full React page renders on Vercel

---
+++ app/home/page.tsx
@@ -0,0 +1,19 @@
+import { Hero } from '@/components/Hero'
+import { FeaturedTools } from '@/components/FeaturedTools'
+import { ToolCategories } from '@/components/ToolCategories'
+import { ValuePropositions } from '@/components/ValuePropositions'
+import { Stats } from '@/components/Stats'
+
+export const dynamic = 'force-dynamic'
+
+export default function HomePage() {
+  return (
+    <>
+      <Hero />
+      <FeaturedTools />
+      <ToolCategories />
+      <ValuePropositions />
+      <Stats />
+    </>
+  )
+}
