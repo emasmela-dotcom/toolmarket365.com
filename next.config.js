24b39e29a9eb515b030b2c2cc3cb54e50b0b4f4d
Eric <emasmela1976@gmail.com>
2026-01-31 16:56:31 -0500

    Full static homepage at / (Hero, Featured Tools, Categories, Value Props, Stats); Strict Mode off; no-store so no cache

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const webpack = require('webpack')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   images: {
     domains: [],
   },
